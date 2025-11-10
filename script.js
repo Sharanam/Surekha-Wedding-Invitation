faea3c2f4728bed342010aa0c378d01022c604bc
Sharanam <sharanamchotai@gmail.com>
2025-11-10 22:49:42 +0530

    fix: typo

--- script.js
+++ script.js
@@ -1,57 +1,56 @@
 const names = [
-  {
-    dhokai: "અતુલભાઈ ધોકાઈ",
-    kamleshmasa: "કમલેશભાઈ કારિયા અને પરિવાર",
-    sata: "સાતા અરવિંદભાઈ અને પરિવાર",
-    raythatha: "દામોદરભાઈ રાયઠઠા અને પરિવાર",
-    kundanfoi: "કારિયા નવીનભાઈ અને પરિવાર",
-    motapappa: "કિરીટભાઈ તથા નરેન્દ્ર લાલચેતા અને પરિવાર",
-    ilafai: "ઇલાબેન હરીશકુમાર દત્તાણી અને પરિવાર",
-    rashmifai: "રાજેશકુમાર ભરતભાઈ સોનછાત્રા અને પરિવાર",
-    simariya: "મંજુલાબેન મથુરાદાસ સીમરીયા અને પરિવાર",
-    vandanamami: "વંદનાબેન દુર્લભજી કોટેચા.",
-    khakhariya: "દિલીપભાઈ પસાભાઇ ખાખરીયા અને પરિવાર",
-    chotai: "વિરેન્દ્રભાઈ તથા કેતનભાઈ ચોટાઈ, સહ પરિવાર.",
-    thakrar: "સુરેશભાઈ શાંતિલાલ ઠકરાર અને પરિવાર",
-    dipakbhai: "દીપકભાઈ શાહ",
-    devendrabhai: "દેવેન્દ્રકુમાર જાદવ અને પરિવાર",
-    nathvani: "મહેન્દ્રભાઈ  કાનજીભાઈ નથવાણી અને પરિવાર",
-    panjvani: "ભરતભાઈ રસિકભાઈ પંજવાણી અને પરિવાર",
-    lakhani: "પ્રવીણભાઈ વસંતભાઈ લાખાણી અને પરિવાર",
-    joshi: "દિલીપભાઈ જોશી અને પરિવાર",
-    agarwal: "Sanjaykumar Agarwal and Family",
-    jagwani: "અમિતભાઇ જગવાણી અને પરિવાર",
-    jagwaniandcompany: "Jagwani and Company",
-    dipikachauhan: "દીપિકા ચૌહાણ",
-    kanabar: "કૃષ્ણકુમાર ભગવાનજીભાઈ કાનાબાર અને પરિવાર",
-    sk: "શશીકાંત મકનભાઈ મકવાણા અને પરિવાર",
-    ak: "Aryaman Pravinbhai Karde and Family",
-    pv: "Parth Nareshbhai Vaswani and Family",
-    noroxen: "Shiv Patel and Family",
-    girls: "Aayushi & Yogita",
-    shailesh: "Shailesh Parmar and Family",
-    friends: "Janki, Radha, Anjali, Noor, Mahima, Amrita, Rafik, OP.",
-    mcom: "Namrata, Janki, Nisha, Aishwarya.",
-    kajal: "કાજલ ચૌહાણ અને પરિવાર",
-    rasula: "રસુલા અને પરિવાર",
-    idxteam:
-      "Simon Varghese, Darshak Shah, Bhavesh Gupta, Dixit Solanki, Deepkumar Patel, Deep Sharma, Ankur Sharma. (with families)",
-  },
+    {
+        kamleshmasa: "કમલેશકુમાર ચંદ્રકાન્તભાઈ  કારિયા અને પરિવાર",
+        sata: "અરવિંદકુમાર પ્રભુદાસભાઈ સાતા અને પરિવાર",
+        raythatha: "દામોદરભાઈ કેશવજી રાયઠઠા અને પરિવાર",
+        kundanfoi: "નવીનકુમાર ઓધવજીભાઈ કારિયા  અને પરિવાર",
+        lalcheta: "કિરીટભાઈ તથા નરેન્દ્રભાઈ  લાલચેતા અને પરિવાર",
+        ilafai: "ઇલાબેન હરીશકુમાર દત્તાણી અને પરિવાર",
+        rashmifai: "રાજેશકુમાર ભરતભાઈ સોનછાત્રા અને પરિવાર",
+        simariya: "મંજુલાબેન મથુરાદાસ સીમરીયા અને પરિવાર",
+        vandanamami: "વંદનાબેન દુર્લભજી કોટેચા.",
+        khakhariya: "દિલીપભાઈ પસાભાઇ ખાખરીયા અને પરિવાર",
+        chotai: "વિરેન્દ્રભાઈ તથા કેતનભાઈ ચોટાઈ, સહ પરિવાર.",
+        thakrar: "સુરેશભાઈ શાંતિલાલ ઠકરાર અને પરિવાર",
+        deepakbhai: "દીપકભાઈ શાહ",
+        devendrabhai: "દેવેન્દ્રકુમાર જાદવ અને પરિવાર",
+        nathvani: "મહેન્દ્રભાઈ  કાનજીભાઈ નથવાણી અને પરિવાર",
+        panjvani: "ભરતભાઈ રસિકભાઈ પંજવાણી અને પરિવાર",
+        lakhani: "પ્રવીણભાઈ વસંતભાઈ લાખાણી અને પરિવાર",
+        joshi: "દિલીપભાઈ જોશી અને પરિવાર",
+        agarwal: "Sanjaykumar Agarwal and Family",
+        jagwani: "અમિતભાઇ જગવાણી અને પરિવાર",
+        jagwaniandcompany: "Jagwani and Company",
+        dipikachauhan: "દીપિકા ચૌહાણ",
+        kanabar: "કૃષ્ણકુમાર ભગવાનજીભાઈ કાનાબાર અને પરિવાર",
+        sk: "શશીકાંત મકનભાઈ મકવાણા અને પરિવાર",
+        ak: "Aryaman Pravinbhai Karde and Family",
+        pv: "Parth Nareshbhai Vaswani and Family",
+        shiv: "Shiv Patel and Family",
+        girls: "Aayushi & Yogita",
+        shailesh: "Shailesh Parmar and Family",
+        friends: "Janki, Radha, Anjali, Noor, Mahima, Amrita, Rafik, OP.",
+        mcom: "Namrata, Janki, Nisha, Aishwarya.",
+        kajal: "કાજલ ચૌહાણ અને પરિવાર",
+        rasula: "રસુલા અને પરિવાર",
+        idxteam:
+            "Simon Varghese, Darshak Shah, Bhavesh Gupta, Dixit Solanki, Deepkumar Patel, Deep Sharma, Ankur Sharma. (with families)",
+    },
 ];
 
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 
 if (id && names[0][id]) {
-  const recipientElement = document.getElementById("recipient-name");
-  if (recipientElement) {
-    recipientElement.textContent = names[0][id];
-  }
+    const recipientElement = document.getElementById("recipient-name");
+    if (recipientElement) {
+        recipientElement.textContent = names[0][id];
+    }
 } else {
-  const p = document.getElementById("invitation-recipient");
-  if (p) {
-    p.remove();
-  }
+    const p = document.getElementById("invitation-recipient");
+    if (p) {
+        p.remove();
+    }
 }
 
 // load each section with animation as the user scrolls down
@@ -59,36 +58,36 @@ const sections = document.querySelectorAll("section");
 const options = { threshold: 0.1 };
 
 const observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      const section = entry.target;
-      section.classList.add("revealed");
-      observer.unobserve(section);
-    }
-  });
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            const section = entry.target;
+            section.classList.add("revealed");
+            observer.unobserve(section);
+        }
+    });
 }, options);
 
 sections.forEach((section) => {
-  observer.observe(section);
+    observer.observe(section);
 });
 
 // Smooth scroll for anchor links
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth",
+        });
     });
-  });
 });
 
 const pageSizeScrollIndicator = document.querySelector(
-  ".page-size-scroll-indicator",
+    ".page-size-scroll-indicator",
 );
 window.addEventListener("scroll", () => {
-  const scrollTop = window.scrollY;
-  const docHeight = document.body.offsetHeight;
-  const winHeight = window.innerHeight;
-  const scrollPercent = (scrollTop / (docHeight - winHeight)) * 100;
-  pageSizeScrollIndicator.style.width = scrollPercent + "%";
+    const scrollTop = window.scrollY;
+    const docHeight = document.body.offsetHeight;
+    const winHeight = window.innerHeight;
+    const scrollPercent = (scrollTop / (docHeight - winHeight)) * 100;
+    pageSizeScrollIndicator.style.width = scrollPercent + "%";
 });
